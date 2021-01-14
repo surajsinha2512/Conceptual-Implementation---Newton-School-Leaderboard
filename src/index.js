1e3c3adfdd811d91d3348018faf197ef2179e6a9
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2021-01-14 18:18:58 +0530

    push code

--- src/index.js
+++ src/index.js
@@ -12,8 +12,17 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 // your code goes here
+app.get("/topRankings", async (req, res) => {
+    const { limit, offset } = req.query;
+    const lim = limit === undefined ? 20 : isNaN(limit) ? 20 : Number(limit);
+    const off = offset === undefined ? 0 : isNaN(offset) ? 0 : Number(offset);
+    const topper = await data.slice(off, off + lim);
+   console.log(req.query);
+  
+    res.send(topper);
+  });
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
-module.exports = app;
+module.exports = app; 
